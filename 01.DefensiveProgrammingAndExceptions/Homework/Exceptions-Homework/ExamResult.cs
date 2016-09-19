daca0b6c42b88bb1e53e3d3d7fe9d64a75f923d1
Nikola Stefanov <stefanov.nm@gmail.com>
2016-09-20 00:50:15 +0300

    ExamResult is done

--- 01.DefensiveProgrammingAndExceptions/Homework/Exceptions-Homework/ExamResult.cs
+++ 01.DefensiveProgrammingAndExceptions/Homework/Exceptions-Homework/ExamResult.cs
@@ -2,39 +2,90 @@
 
 public class ExamResult
 {
+    private const int MinGradeValue = 0;
+
+    private int grade;
+    private int minGrade;
+    private int maxGrade;
+    private string comments;
+
     public ExamResult(int grade, int minGrade, int maxGrade, string comments)
     {
-        if (grade < 0)
-        {
-            throw new Exception();
-        }
-
-        if (minGrade < 0)
-        {
-            throw new Exception();
-        }
-
-        if (maxGrade <= minGrade)
-        {
-            throw new Exception();
-        }
-
-        if (comments == null || comments == string.Empty)
-        {
-            throw new Exception();
-        }
-
         this.Grade = grade;
         this.MinGrade = minGrade;
         this.MaxGrade = maxGrade;
         this.Comments = comments;
     }
 
-    public int Grade { get; private set; }
+    public int Grade
+    {
+        get
+        {
+            return this.grade;
+        }
 
-    public int MinGrade { get; private set; }
+        private set
+        {
+            if (value < this.MinGrade)
+            {
+                throw new ArgumentOutOfRangeException("Grade must be greater than the min grade!");
+            }
 
-    public int MaxGrade { get; private set; }
+            this.grade = value;
+        }
+    }
 
-    public string Comments { get; private set; }
+    public int MinGrade
+    {
+        get
+        {
+            return this.minGrade;
+        }
+
+        private set
+        {
+            if (value < MinGradeValue)
+            {
+                throw new ArgumentOutOfRangeException("Min value should be equal or greater than " + MinGradeValue + "!");
+            }
+
+            this.minGrade = value;
+        }
+    }
+
+    public int MaxGrade
+    {
+        get
+        {
+            return this.maxGrade;
+        }
+
+        private set
+        {
+            if (value <= this.MinGrade)
+            {
+                throw new ArgumentOutOfRangeException("Max grade must be greater than min grade!");
+            }
+
+            this.maxGrade = value;
+        }
+    }
+
+    public string Comments
+    {
+        get
+        {
+            return this.comments;
+        }
+
+        private set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Comment should not be empty!");
+            }
+
+            this.comments = value;
+        }
+    }
 }
