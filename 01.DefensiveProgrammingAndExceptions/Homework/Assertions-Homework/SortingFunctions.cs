f59d46b43ba7e2f70bc1aed31eab0b13cc3be922
Nikola Stefanov <stefanov.nm@gmail.com>
2016-09-19 23:42:16 +0300

    split to classes, and partly implemented

---
+++ 01.DefensiveProgrammingAndExceptions/Homework/Assertions-Homework/SortingFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AssertionsHomework
+{
+    public class SortingFunctions
+    {
+        internal static void SelectionSort<T>(T[] arr) where T : IComparable<T>
+        {
+            for (int index = 0; index < arr.Length - 1; index++)
+            {
+                int minElementIndex = FindMinElementIndex(arr, index, arr.Length - 1);
+                Swap(ref arr[index], ref arr[minElementIndex]);
+            }
+        }
+
+        private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex)
+        where T : IComparable<T>
+        {
+            int minElementIndex = startIndex;
+            for (int i = startIndex + 1; i <= endIndex; i++)
+            {
+                if (arr[i].CompareTo(arr[minElementIndex]) < 0)
+                {
+                    minElementIndex = i;
+                }
+            }
+
+            return minElementIndex;
+        }
+
+        private static void Swap<T>(ref T x, ref T y)
+        {
+            T oldX = x;
+            x = y;
+            y = oldX;
+        }
+    }
+}
